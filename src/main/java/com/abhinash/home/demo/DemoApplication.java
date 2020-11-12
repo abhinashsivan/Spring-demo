bceaab0d327ba995c365c7c87576e5cbb1d1c4df
abhinashsivan <qwerty@00098>
2020-11-12 12:50:08 +0530

    commit-1;demo;helloworld

---
+++ src/main/java/com/abhinash/home/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.abhinash.home.demo;
+import java.util.*;
+import java.io.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
